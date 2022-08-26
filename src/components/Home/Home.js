00654f0ef5d642a3a97e2effaae0ceb0d1f8d6f0
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-27 00:02:21 +0600

    data load

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 
 const Home = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/region/asia')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [countries])
+
     return (
         <div>
-            <h2>Home</h2>
+            {
+                countries.map((country, index) => <Product
+                    key={index}
+                    country={country}
+                ></Product>)
+            }
         </div>
     );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
-const Product = () => {
+
+const Product = ({ country }) => {
+    const { name, flags, ccn3 } = country;
+    const navigate = useNavigate();
+
+    const showDetails = (ccn3) => {
+        const path = `/product/${ccn3}`
+        navigate(path);
+    }
+
     return (
         <div>
-            <h2>Product</h2>
+            <img width={200} height={150} src={flags.png} alt="" />
+            <p>{name.official}</p>
+            <button>{name.common}</button>
         </div>
     );
 };
