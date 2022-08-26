60f44aeb837e6a42a61935d87826f0fef6da98cf
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-26 23:08:02 +0600

    route and path element set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import { Routes, Route, } from "react-router-dom";
+import Home from './components/Home/Home';
+import Shop from './components/Shop/Shop';
+import Product from './components/Product/Product';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Routes>
-
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/shop' element={<Shop></Shop>} ></Route>
+        <Route path='/product' element={<Product></Product>} ></Route>
+        <Route path='/about' element={<About></About>} ></Route>
+        <Route path='*' element={<NotFound></NotFound>} ></Route>
       </Routes>
     </div>
   );
