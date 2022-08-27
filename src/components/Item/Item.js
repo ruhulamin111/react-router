7e5f964cb09f0d2bd77ed55ba463296759c5b63f
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-27 10:25:31 +0600

    nested routing

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -9,14 +9,15 @@ const Item = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setItems(data[0]))
-    }, [items])
-    console.log(items);
+    }, [productId])
+
     return (
         <div>
             <img height={200} src={items.flags?.png} alt="" />
-            <h3>{items.name}</h3>
-            <h3>{items.region}</h3>
-            <h3>{items.population}</h3>
+            <h3>Name : {items.name}</h3>
+            <h3>Area : {items.area}</h3>
+            <h3>Region : {items.region}</h3>
+            <h3>Population : {items.population}</h3>
 
         </div>
     );
--- src/components/MealDbDetails/MealDbDetails.js
+++ src/components/MealDbDetails/MealDbDetails.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const MealDbDetails = () => {
     return (
         <div>
-            <h2>Home</h2>
 
         </div>
     );
 };
 
-export default Home;
+export default MealDbDetails;
