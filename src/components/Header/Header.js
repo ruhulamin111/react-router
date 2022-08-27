c73e990a7972e85f7ce460e5ea4b3ae0da850510
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-27 07:55:35 +0600

    add active link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-
-
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            textDecoration: isActive ? 'underline' : 'none',
+            color: isActive ? "red" : "",
+        };
+    }
 
     return (
         <div>
             <nav>
-                <Link to="/home">Home</Link>
-                <Link to="/shop">Shop</Link>
-                <Link to="/about">About</Link>
+                <NavLink style={navLinkStyles} to="/home">Home</NavLink>
+                <NavLink style={navLinkStyles} to="/shop">Shop</NavLink>
+                <NavLink style={navLinkStyles} to="/about">About</NavLink>
             </nav>
         </div >
     );
